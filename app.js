a607fc798bd427e4fd80ee0303107a1ae84476ce
Ömer <108544791+themrth@users.noreply.github.com>
2022-08-25 15:46:45 +0300

    database connection established, model created, data extracted from database and sent to home page

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 const app = express();
 const router = express.Router();
 const path = require('path');
 
 app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.urlencoded({extended: true}))
+app.use(express.json());
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
@@ -11,6 +14,15 @@ app.set('view engine', 'ejs');
 const userRoutes = require('./routes/user');
 app.use(userRoutes);
 
+// MongoDB Connection
+mongoose.connect('mongodb://localhost/cleanblog-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+
+
+// Local Connection
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is starting on ${port}`);
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+// MODELS
+const Post = require('../models/Post');
+
+
 // Routes
-router.get('/', (req, res) => {
-  res.render('../views/main.ejs');
+router.get('/', async (req, res) => {
+  const posts = await Post.find({})
+  res.render('../views/main.ejs', {
+    posts
+  });
 });
 
 router.get('/about', (req, res) => {
@@ -17,5 +24,10 @@ router.get('/post', (req, res) => {
 router.get('/add-post', (req, res) => {
   res.render('../views/add_post.ejs');
 });
+
+router.post('/posts', async (req,res) => {
+  await Post.create(req.body)
+  res.redirect('/')
+})
 
 module.exports = router;
